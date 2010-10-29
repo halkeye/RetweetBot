51db6b959227aea640c65938f84a3e3071f05b03
Gavin <halkeye@gmail.com>
2010-10-29 04:40:51 +0000

    Switching to Net::Twitter in hopes it 'dies' less often as its supposed to auto-retry

--- retweetbot.pl
+++ retweetbot.pl
@@ -1,26 +1,8 @@
 #!/usr/bin/perl 
-#===============================================================================
-#
-#         FILE:  vanPortGamers-TwitBot.pl
-#
-#        USAGE:  ./vanPortGamers-TwitBot.pl 
-#
-#  DESCRIPTION:  
-#
-#      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:  Gavin Mogan (Gavin), <gavin@kodekoan.com>
-#      COMPANY:  KodeKoan
-#      VERSION:  1.0
-#      CREATED:  08/20/2010 08:25:00 PM UTC
-#     REVISION:  ---
-#===============================================================================
-
 use strict;
 use warnings;
-use Net::Twitter::Lite;
+
+use Net::Twitter;
 use Data::Dumper;
 use File::HomeDir;
 use File::Spec;
@@ -28,12 +10,8 @@ use Config::Std;
 use Carp;
 use Getopt::Std;
 
-$SIG{__DIE__} = sub {
-    die @_ if $^S;
-    return if ($_[0] =~ /502: Bad Gateway/);
-    die @_;
-};
 my %opts;
+$opts{v} = 1 if $ENV{VIM};
 getopts('vc:', \%opts);
 
 $Carp::Verbose = 1;
@@ -45,79 +23,72 @@ if (-e $confFile)
 }
 
 # apt-get install libconfig-std-perl libnet-twitter-lite-perl libfile-homedir-perl libnet-oauth-perl
-my $nt = Net::Twitter::Lite->new(
-        consumer_key    => $conf{default}{consumer_key},
-        consumer_secret => $conf{default}{consumer_secret},
-);
-
-# You'll save the token and secret in cookie, config file or session database
-if ($conf{default}{access_token} && $conf{default}{access_token_secret}) {
-  $nt->access_token($conf{default}{access_token});
-  $nt->access_token_secret($conf{default}{access_token_secret});
-}
-
-unless ( $nt->authorized ) {
-  # The client is not yet authorized: Do it now
-  print "Authorize this app at ", $nt->get_authorization_url, " and enter the PIN#\n";
-
-  my $pin = <STDIN>; # wait for input
-  chomp $pin;
-
-  ($conf{default}{access_token}, $conf{default}{access_token_secret}) = $nt->request_access_token(verifier => $pin);
-  write_config %conf, $confFile;
-}
-
-my $limit;
+my $nt;
 eval {
-    $limit = $nt->rate_limit_status();
+    $nt = Net::Twitter->new(
+        traits              => [qw/OAuth API::REST API::Search/],
+        consumer_key        => $conf{default}{consumer_key},
+        consumer_secret     => $conf{default}{consumer_secret},
+        access_token        => $conf{default}{access_token},
+        access_token_secret => $conf{default}{access_token_secret},
+        source              => 'api',
+    );
 };
-die ("Error getting limit: $@") if $@;
+if ( my $err = $@ ) {
+    die $@ unless blessed $err && $err->isa('Net::Twitter::Error');
+
+    warn "HTTP Response Code: ", $err->code, "\n",
+    "HTTP Message......: ", $err->message, "\n",
+    "Twitter error.....: ", $err->error, "\n";
+}
+
+my $limit = $nt->rate_limit_status();
 unless ($limit->{remaining_hits})
 {
-    print "no more api allowed\n";
-    exit;
+    die("no more api allowed\n");
 }
-#print "We've got $limit->{remaining_hits} API calls left\n"
 
 my $didChange = 0;
 
 foreach my $term (grep { /^\#/ } keys %conf)
 {
     $conf{$term}{lastSeenId} ||= 0;
-    my $status;
-    warn "Searching for $term\n" if $opts{v};
-    my $r = $nt->search({
-            since_id=>$conf{$term}{lastSeenId},
-            q=>$term
-    });
-        for $status ( @{$r->{results}} ) {
+    eval {
+        my $status;
+        warn "Searching for '$term'\n" if $opts{v};
+        my $r = $nt->search({q=>$term, since_id=> $conf{$term}{lastSeenId}});
+        #my $r = $nt->search({q=>$term});
+        for $status ( @{$r->{results}} ) 
+        {
             next unless $status;
             next unless $status->{id} > $conf{$term}{lastSeenId};
             # If its my tweet, ignore it
             if ($status->{'from_user'} ne $conf{default}{username})
             {
-                eval {
-                    $nt->retweet($status->{id});
-                };
-                if ($@)
-                {
-                    warn ("Error handling $term: $@ -- " . Data::Dumper::Dumper($status));
-                }
+                $nt->retweet($status->{id});
             }
             $conf{$term}{lastSeenId} = $status->{id};
             $didChange = 1;
         }
+    };
+    if ( my $err = $@ ) 
+    {
+        die $@ unless blessed $err && $err->isa('Net::Twitter::Error');
+
+        warn "Searching for $term $conf{$term}{lastSeenId}\n",
+        "HTTP Response Code: ", $err->code, "\n",
+        "HTTP Message......: ", $err->message, "\n",
+        "Twitter error.....: ", $err->error, "\n";
+    }
 }
 
 if (0)
 {
-    eval {
-        my $r = $nt->direct_messages();
-        for my $dm ( @$r ) {
-            $nt->new_direct_message({user=>"halkeye", text=>"\@$dm->{sender_screen_name} - $dm->{text}"});
-            $nt->destroy_direct_message($dm->{id});
-        }
-    };
+    my $r = $nt->direct_messages();
+    for my $dm ( @$r ) {
+        $nt->new_direct_message({user=>"halkeye", text=>"\@$dm->{sender_screen_name} - $dm->{text}"});
+        $nt->destroy_direct_message($dm->{id});
+    }
     warn("Error handling dms: $@") if $@;
 }
 
@@ -127,4 +98,3 @@ if ($didChange)
 }
 
 1;
-
